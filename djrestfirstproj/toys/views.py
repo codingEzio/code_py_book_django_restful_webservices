23738510a62ef181d96adde2e55c67bd50f4dd52
Ezio <assassinste@gmail.com>
2019-10-02 09:19:54 +0800

    Implementing 'PUT' API for toys

--- djrestfirstproj/toys/views.py
+++ djrestfirstproj/toys/views.py
@@ -62,3 +62,27 @@ def toy_detail(request, pk):
         # $ curl -iX GET localhost:8000/toys/1
         toy_serializer = ToySerializer(toy)
         return JSONResponse(toy_serializer.data)
+
+    elif request.method == 'PUT':
+        # Basically the same as `POST` but with 'pk' to update specific one
+
+        # http PUT :8000/toys/4 \
+        #   name="Sokoban" \
+        #   description="a mobile game" \
+        #   toy_category="Electronics" \
+        #   was_included_in_home=false \
+        #   release_date="2017-03-12T09:09:00.656665Z"
+
+        # curl -iX PUT -H "Content-Type: application/json"
+        #   -d '{"name":"Tombraider","description":"a mobile game",
+        #   "toy_category":"Electronics","was_included_in_home":"false",
+        #   "release_date":"2016-05-25T01:01:00.652465Z"}' localhost:8000/toys/5
+        toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(toy, data=toy_data)
+
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return JSONResponse(toy_serializer.data)
+
+        return JSONResponse(toy_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
