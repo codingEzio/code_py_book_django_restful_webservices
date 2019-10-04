505059c989c2c7c7a051066cac4914e22fe939d7
Ezio <assassinste@gmail.com>
2019-10-05 00:00:47 +0800

    Adding pagination feature

--- djrestfirstproj/restful/settings.py
+++ djrestfirstproj/restful/settings.py
@@ -109,3 +109,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST Framework configurations
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'drones.custompagination.LimitOffsetPaginationWithUpperBound',
+
+    'PAGE_SIZE'               : 3,
+}
