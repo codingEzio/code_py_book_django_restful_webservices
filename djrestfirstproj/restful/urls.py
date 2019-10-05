7623f0d52c1892a7c693d25fad9330ad828f7284
Ezio <assassinste@gmail.com>
2019-10-05 22:18:50 +0800

    Adding browsable APIs

--- djrestfirstproj/restful/urls.py
+++ djrestfirstproj/restful/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
 
     # url(r'^/', include('toys.urls')),
     url(r'^', include('drones.urls')),
