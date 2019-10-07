2f0442d2f4d49eeb166debc185eee1786522bb27
Ezio <assassinste@gmail.com>
2019-10-07 10:59:20 +0800

    A word about 'impl versioning APIs'

--- djrestfirstproj/restful/urls.py
+++ djrestfirstproj/restful/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
+    # Just saying, this might not the modern way of doing it
+    # but this one is quite intuitive to me (vN for view, vN/api-xx/ for login|out)
+    # see 'https://github.com/search?l=Python&q=NamespaceVersioning&type=Code'
     url(r'^v1/', include('drones.urls', namespace='v1')),
     url(r'^v1/api-auth/', include('rest_framework.urls', namespace='rest_v1')),
     url(r'^v2/', include('drones.v2.urls', namespace='v2')),
